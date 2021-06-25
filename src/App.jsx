21b397060fcc3eb0e87c6262d04c997366933663
Eliezer516 <ybarretomaterano@gmail.com>
2021-06-24 21:08:16 -0400

    esqueleto creado

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {HolaMundo} from './components/HolaMundo.jsx'
+
+export function App() {
+	return (
+		<div>
+			<HolaMundo />
+		</div>
+	)
+}
---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function HolaMundo() {
+	return (
+		<div>
+			<h1>Hola mundo</h1>
+		</div>
+	)
+}
